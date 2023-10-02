7d9fae3f0ff1052f47444d320ab2b1b6ad64e78c
Bonfanti Cecilia <bonfanticeciliaemilce@gmail.com>
2023-10-02 13:10:45 -0300

    se actualiza logica js de calculadora

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const botonesOperadores = document.querySelectorAll('.operador');
 const display = new Display (displayValorAnterior,displayValorActual);
 
 botonesNumeros.forEach(boton => boton.addEventListener('click', () =>{display.agregarNumero(boton.innerHTML)}));
+
+botonesOperadores.forEach(boton =>{
+boton.addEventListener('click', ()=> display.computar(boton.value))
+});
